6a421c30f36b9b855f7690f4322ba2591c2dc680
Gianluca Pegoraro <gpegoraro@fastmail.fm>
2026-02-12 14:02:34 -0500

    Add remaining figures from Martin's code

---
+++ Fig_S1C/Stat_analysis/marascuilo_contacts.R
@@ -0,0 +1,69 @@
+# Load the required library
+library(dplyr)
+library(readr)
+library(here)
+
+#Set local directory
+local_dir <- here("Fig_S1C/Stat_analysis")
+
+# Read input CSV file located in same folder as script
+data <- read.csv(file.path(local_dir, "IDR3-TCF3-time-course.csv"))
+data <- na.omit(data)
+
+# Create an empty matrix to store absolute differences
+diff_table <- matrix(NA, nrow = nrow(data), ncol = nrow(data))
+
+# Generate differences table
+# Iterate through all possible pairs and calculate the absolute difference
+for (i in 1:nrow(data)) {
+  for (j in 1:nrow(data)) {
+    diff_table[i, j] <- abs(data$p[i] - data$p[j])
+  }
+}
+# Create a new data frame with the results
+diff_table_df <- as.data.frame(diff_table)
+row.names(diff_table_df) <- data$Condition
+colnames(diff_table_df) <- data$Condition
+
+#Generate critical range table
+# Chi square critical value
+confidence <- 0.999 ####p-value=0.001
+DF <- nrow(data)-1
+chisqcv <- qchisq(confidence,DF)
+# Create an empty matrix to store the calculated values
+cr_table <- matrix(NA, nrow = nrow(data), ncol = nrow(data))
+# Iterate through all possible pairs and calculate the values
+for (i in 1:nrow(data)) {
+  for (j in 1:nrow(data)) {
+    pi <- data$p[i]
+    pj <- data$p[j]
+    Ni <- data$N[i]
+    Nj <- data$N[j]
+    cr_table[i, j] <- sqrt(chisqcv) * sqrt((pi*(1-pi)/Ni) + (pj*(1-pj)/Nj))
+  }
+}
+# Create a new data frame with the results
+cr_table_df <- as.data.frame(cr_table)
+row.names(cr_table_df) <- data$Condition
+colnames(cr_table_df) <- data$Condition
+
+#Compare differences table with critical range table
+# Create an empty matrix to store the comparison results
+comparison_matrix <- matrix(NA, nrow = nrow(diff_table_df), ncol = ncol(diff_table_df))
+# Iterate through each position and compare values
+for (i in 1:nrow(diff_table_df)) {
+  for (j in 1:ncol(diff_table_df)) {
+    if (diff_table_df[i, j] > cr_table_df[i, j]) {
+      comparison_matrix[i, j] <- "S"
+    } else {
+      comparison_matrix[i, j] <- "NS"
+    }
+  }
+}
+# Create a new data frame with the comparison results
+comparison_df <- as.data.frame(comparison_matrix)
+row.names(comparison_df) <- data$Condition
+colnames(comparison_df) <- data$Condition
+
+#Write output files
+write.csv(comparison_df, file.path(local_dir,"Output/Contacts_Marascuilo_results.csv"))
---
+++ Fig_S4C/Stat_analysis/2_way_anova_RNAspots.R
@@ -0,0 +1,33 @@
+# Load the necessary packages
+library(here)
+
+#Set local directory
+local_dir <- here("Fig_S4C/Stat_analysis")
+
+# Read input CSV file located in same folder as script
+data <- read.csv(file.path(local_dir, "HeLa-RNAF-spots.csv"))
+data <- na.omit(data)
+
+# Perform the two-factor ANOVA
+model <- aov(dependent_variable ~ factor1 * factor2, data = data)
+
+# Check the ANOVA table
+anova_table <- anova(model)
+write.csv(anova_table, file.path(local_dir,"Output/count-anova.csv"))
+
+# Conduct post hoc tests if significant interaction or main effects are found
+if (anova_table$"Pr(>F)"[1] < 0.05 || anova_table$"Pr(>F)"[2] < 0.05 || anova_table$"Pr(>F)"[3] < 0.05) {
+  posthoc_factor1 <- TukeyHSD(aov(dependent_variable ~ factor1, data = data))
+  posthoc_factor2 <- TukeyHSD(aov(dependent_variable ~ factor2, data = data))
+  posthoc_interaction <- TukeyHSD(aov(dependent_variable ~ factor1 * factor2, data = data))
+
+  #Convert to data frames  
+  posthoc_factor1_df <- as.data.frame(posthoc_factor1$factor1)
+  posthoc_factor2_df <- as.data.frame(posthoc_factor2$factor2)
+  posthoc_interaction_df <- as.data.frame(posthoc_interaction$`factor1:factor2`)
+  
+  #Write output files
+  write.csv(posthoc_factor1_df, file.path(local_dir,"Output/count-posthoc_factor1.csv"))
+  write.csv(posthoc_factor2_df, file.path(local_dir,"Output/count-posthoc_factor2.csv"))
+  write.csv(posthoc_interaction_df, file.path(local_dir,"Output/count-posthoc_interaction.csv"))
+}
